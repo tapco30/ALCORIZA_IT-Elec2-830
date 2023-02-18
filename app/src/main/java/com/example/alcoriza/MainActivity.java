0bb9bc9a146564c62d3aca1e7e9fa53eee013ca6
Admin <tapcoalcoriza17@gmail.com>
2023-02-18 13:09:59 +0800

    SecondACTIVITY

--- app/src/main/java/com/example/alcoriza/MainActivity.java
+++ app/src/main/java/com/example/alcoriza/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.alcoriza;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "This is a verbose log.");
+        Log.d(TAG, "This is a debug log.");
+        Log.i(TAG, "This is a info log.");
+        Log.w(TAG, "This is a warn log.");
+        Log.e(TAG, "This is a error log.");
+
+        Button button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/alcoriza/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.example.alcoriza;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button Btn = (Button) findViewById(R.id.button3);
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(SecondActivity.this, SecondActivity.class);
+            }
+        });
+    }
+}
