33fc7dda22d43239efbd90a34c9d4b8c8551b52c
Admin <tapcoalcoriza17@gmail.com>
2023-03-11 11:49:21 +0800

    Activity1-fragment

--- app/src/main/java/com/example/alcoriza/Activityfour.java
+++ app/src/main/java/com/example/alcoriza/Activityfour.java
@@ -1,8 +1,14 @@
 package com.example.alcoriza;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Activityfour extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class Activityfour extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activityfour);
+
+        Button Btn = (Button) findViewById(R.id.button5);
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(Activityfour.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/alcoriza/ThirdActivity.java
+++ app/src/main/java/com/example/alcoriza/ThirdActivity.java
@@ -1,8 +1,14 @@
 package com.example.alcoriza;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        Button Btn = (Button) findViewById(R.id.button7);
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button Clicked");
+                Intent intent = new Intent(ThirdActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
